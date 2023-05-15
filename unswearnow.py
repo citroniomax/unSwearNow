4129266a3579e87ee558b980e546e05d93075f23
citron <68706915+citroniomax@users.noreply.github.com>
2023-05-15 18:13:19 +0200

    Update unswearnow.py

--- unswearnow.py
+++ unswearnow.py
@@ -1,7 +1,7 @@
 from pynput import keyboard
 k = keyboard
 
-keylog = ''
+keylog = ' '
 file = open('filter.txt', 'r', encoding="utf-8").readlines()
 
 for i in range(len(file)):      # convert the txt file into a list of list
@@ -18,6 +18,7 @@ for i in range(len(file)):      # convert the txt file into a list of list
 
 def down(key):     # main function
     global keylog
+    insult = ''
 
     if len(str(key)) == 3:      # key management
         keylog += str(key)[1:2:]
@@ -26,14 +27,22 @@ def down(key):     # main function
     if str(key) == 'Key.backspace':
         keylog = keylog[:-1:]
     if str(key) == 'Key.enter':
-        keylog = ''
+        keylog = ' '
+    if keylog == '':
+        keylog = ' '
     
     for i in range(len(file)):       # word changer
-        if file[i][0] in keylog:
-            for _ in range(len(file[i][0])):
-                k.Controller().press(keyboard.Key.backspace)
-                k.Controller().release(keyboard.Key.backspace)
-            k.Controller().type(file[i][1])
+        fx = file[i][0]
+        if fx in keylog:
+            if keylog[-len(fx)-1:-len(fx):] == ' ':
+                for _ in range(len(file[i][0])):
+                    k.Controller().press(keyboard.Key.backspace)
+                    k.Controller().release(keyboard.Key.backspace)
+                k.Controller().type(file[i][1])
+            else:
+                for _ in range(len(fx)):        # avoid bug
+                    insult += '-'
+                keylog = keylog[:-len(fx):] + insult
 
     # print(keylog)
 
